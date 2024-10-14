95bf1a886fff240273d821137c8e7d7200cb16f0
fersayago <sayago.fernando@gmail.com>
2024-10-14 17:20:50 -0300

    style: :rotating_light: fix lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="flex gap-4">
+      <Button>Asd</Button>
+      <Button variant="destructive">Asd</Button>
+      <Button variant="secondary">Asd</Button>
+      <Button variant="ghost">Asd</Button>
+      <Button variant="muted">Asd</Button>
+      <Button variant="teritary">Asd</Button>
     </div>
   );
 }
